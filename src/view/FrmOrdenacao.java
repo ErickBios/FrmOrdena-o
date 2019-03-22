43fdfede0ee0cfcac7272ccefde7e0241454bd78
ErickBios <43616117+ErickBios@users.noreply.github.com>
2019-03-22 09:55:52 -0300

    Update FrmOrdenacao.java

--- src/view/FrmOrdenacao.java
+++ src/view/FrmOrdenacao.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Alunos :  Erick Silva de Oliveira.
+ *Alunos : Thiago Antônio de Carvalho Mota
  */
 package view;
 
